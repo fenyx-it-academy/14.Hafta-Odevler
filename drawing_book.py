40068912936c962116ad4b8c78c05be8edf0557b
naimucar <51508685+naimucar@users.noreply.github.com>
2019-09-19 09:24:50 +0200

    Update drawing_book.py

--- drawing_book.py
+++ drawing_book.py
@@ -2,7 +2,11 @@ n=int(input('sayfa sayisini giriniz :'))
 p=int(input('gitmek istediginiz sayfaya giriniz :'))
 x1=n//2
 k=(n-p)//2
-if k<(x1-k):
+if n<=2:
+    print(0)
+elif n%2==0 and n-p==1:
+    print(1)
+elif k<(x1-k):
 
     print(k)
 else:
