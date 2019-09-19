fba70866f2bb4e191a72d68b152ea28bb39ec05c
raktas.py <51508732+nsraktasnsr@users.noreply.github.com>
2019-09-19 23:00:09 +0200

    Update 14.hafta...1.odev.py

--- 14.hafta...1.odev.py
+++ 14.hafta...1.odev.py
@@ -1,14 +1,16 @@
 def pageCount():
     n = int(input())
     p = int(input())
-    if (n - p) < p and n % 2 == 0:      # n double and starting from the end of the book
-        print(int(((n - 1) - p) / 2))
-    elif (n - p) < p and n % 2 != 0:    # n single and starting from the end of the book
-        print(int((n - p) / 2))
-    elif (n - p) > p and n % 2 == 0:    # starting from the beginning of the book
-        print(int(p / 2))
-    elif (n - p) < p and n % 2 != 0:    # starting from the beginning of the book
-        print(int(p / 2))
-    elif n == p:
-        print(int(n - p))
+    if n == p:
+        print(n - p)
+    elif (n - p) >= p :     # starting from the beginning of the book
+        if n % 2 == 0 :
+            print(int(p / 2))
+        else:
+            print(int(p / 2))
+    elif (n - p) <= p :      # starting from the end of the book
+        if n % 2 == 0 :
+            print(int(((n - 1) - p) / 2 )+1)
+        else:
+            print(int((n - p) / 2))
 pageCount()
