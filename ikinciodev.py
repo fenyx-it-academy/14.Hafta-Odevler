1a5aadcf4899cd7490c04bd871592aeb5bd88fce
zerin <zerinmac@zerin-air.home>
2019-09-26 23:30:15 +0200

    14.Hafta-odevler-zerin

--- ikinciodev.py
+++ ikinciodev.py
@@ -1,28 +1,28 @@
-# Complete the formingMagicSquare function below.
-def formingMagicSquare(s):
-    matris = [[[8,1,6], [3,5,7], [4,9,2]],
-              [[6,1,8], [7,5,3], [2,9,4]],
-              [[4,9,2], [3,5,7], [8,1,6]],
-              [[2,9,4], [7,5,3], [6,1,8]],
-              [[8,3,4], [1,5,9], [6,7,2]],
-              [[4,3,8], [9,5,1], [2,7,6]],
-              [[6,7,2], [1,5,9], [8,3,4]],
-              [[2,7,6], [9,5,1], [4,3,8]]]
-    s = []
+import sys
 
-    for k in matris:
-        m = 0
-        fark = []
-        for i in range (3):
-            for j in range (3):
-                fark.append(abs(s[i][j] - k[i][j]))
-        for i in fark:
-            m += i
-        s.append(m)
-    return min(s)
+matrix_list = [[[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+               [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+               [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+               [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
+               [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+               [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
+               [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
+               [[2, 7, 6], [9, 5, 1], [4, 3, 8]]]
+
+
+def get_min_cost(mat: list) -> int:
+    cost_list = [sys.maxsize] * len(matrix_list)
+    for ref_mat in matrix_list:
+        cost = 0
+        for x in range(0, len(mat)):
+            for y in range(0, len(mat)):
+                if mat[x][y] != ref_mat[x][y]:
+                    cost += abs(mat[x][y] - ref_mat[x][y])
+        cost_list.append(cost)
+    return min(cost_list)
 
 s = []
-for _ in range(3):
-    s.append(list(map(int, input().rstrip().split())))
-print(s)
-print(formingMagicSquare(s))
+for s_i in range(3):
+    s_t = [int(s_temp) for s_temp in input().strip().split(' ')]
+    s.append(s_t)
+print(get_min_cost(s))
